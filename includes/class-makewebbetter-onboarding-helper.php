50455a254d5fa865d84f49cb01905e1468db0e73
mohddanish <mohddanish@makewebbetter.com>
2022-01-20 10:58:26 +0530

    onboarding-form-code

--- includes/class-makewebbetter-onboarding-helper.php
+++ includes/class-makewebbetter-onboarding-helper.php
@@ -50,7 +50,7 @@ class Makewebbetter_Onboarding_Helper {
 	 * @since 1.0.0
 	 * @var string Portal id.
 	 */
-	private static $portal_id = '6493626';
+	private static $portal_id = '25444144';
 
 	/**
 	 * Form id of hubspot api.
@@ -58,14 +58,14 @@ class Makewebbetter_Onboarding_Helper {
 	 * @since 1.0.0
 	 * @var string Form activation id.
 	 */
-	private static $onboarding_form_id = 'd94dcb10-c9c1-4155-a9ad-35354f2c3b52';
+	private static $onboarding_form_id = '2a2fe23c-0024-43f5-9473-cbfefdb06fe2';
 	/**
 	 * Form id of hubspot api.
 	 *
 	 * @since 1.0.0
 	 * @var string Form deactivation id.
 	 */
-	private static $deactivation_form_id = '329ffc7a-0e8c-4e11-8b41-960815c31f8d';
+	private static $deactivation_form_id = '67feecaa-9a93-4fda-8f85-f73168da2672';
 
 
 	/**
