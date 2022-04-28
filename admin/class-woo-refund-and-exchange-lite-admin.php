a0eeb3f1840be7914b38b98de7d97bbad3f815df
mohddanish <mohddanish@makewebbetter.com>
2022-04-28 10:24:07 +0530

    phpcs-fixes

--- admin/class-woo-refund-and-exchange-lite-admin.php
+++ admin/class-woo-refund-and-exchange-lite-admin.php
@@ -84,7 +84,8 @@ class Woo_Refund_And_Exchange_Lite_Admin {
 			wp_enqueue_style( 'wps-datatable-css', WOO_REFUND_AND_EXCHANGE_LITE_DIR_URL . 'package/lib/datatables/media/css/jquery.dataTables.min.css', array(), $this->version, 'all' );
 		}
 		if ( isset( $screen->id ) && 'shop_order' === $screen->id ) {
-			wp_enqueue_style( $this->plugin_name, WOO_REFUND_AND_EXCHANGE_LITE_DIR_URL . 'admin/css/woo-refund-and-exchange-lite-order-edit-page.min.css', array(), $this->version, 'all' );
+			// wp_enqueue_style( $this->plugin_name, WOO_REFUND_AND_EXCHANGE_LITE_DIR_URL . 'admin/css/woo-refund-and-exchange-lite-order-edit-page.min.css', array(), $this->version, 'all' );
+			wp_enqueue_style( $this->plugin_name, WOO_REFUND_AND_EXCHANGE_LITE_DIR_URL . 'admin/css/wps-order-edit-page-lite.scss.min.css', array(), $this->version, 'all' );
 		}
 	}
 
@@ -438,10 +439,12 @@ class Woo_Refund_And_Exchange_Lite_Admin {
 									$setting = sanitize_text_field( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ) );
 									$setting = trim( preg_replace( '/\s\s+/', ' ', $setting ) );
 								}
-								if ( 'wps_rma_refund_rules_editor' === $wrael_genaral_setting['id'] ) {
+								if ( 'wps_rma_refund_rules_editor' === $wrael_genaral_setting['id']  ) {
 									update_option( 'wps_rma_refund_rules_editor', wp_kses_post( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ) ) );
+								} elseif ( 'wps_rma_refund_form_css' === $wrael_genaral_setting['id'] ) {
+									update_option( 'wps_rma_refund_form_css', wp_kses_post( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ) ) );
 								} else {
-									update_option( sanitize_text_field( wp_unslash( $wrael_genaral_setting['id'] ) ), is_array( $_POST[ $wrael_genaral_setting['id'] ] ) ? $this->wps_sanitize_array( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ) ) : stripslashes( sanitize_text_field( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ) ) ) );
+									update_option( sanitize_text_field( wp_unslash( $wrael_genaral_setting['id'] ) ), is_array( $_POST[ $wrael_genaral_setting['id'] ] ) ? map_deep( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ), 'sanitize_text_field' ) : stripslashes( sanitize_text_field( wp_unslash( $_POST[ $wrael_genaral_setting['id'] ] ) ) ) );
 								}
 							} else {
 								update_option( sanitize_text_field( wp_unslash( $wrael_genaral_setting['id'] ) ), '' );
