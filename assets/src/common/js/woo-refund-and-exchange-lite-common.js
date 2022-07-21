38092bbf2c1d271153f4dfc7e25667ff7ed36307
mohddanish <mohddanish@makewebbetter.com>
2022-07-21 11:15:44 +0530

    lang-update

--- assets/src/common/js/woo-refund-and-exchange-lite-common.js
+++ assets/src/common/js/woo-refund-and-exchange-lite-common.js
@@ -208,6 +208,9 @@ jQuery( document ).on( 'ready', function(){
 				alerthtml = '<p class="wps_order_msg_sent_notice">'+ wrael_common_param.no_file_attached +'</p><a href="" class="wps_remove_notice_msg">X</a>';
 			}
 		}
+		if ( order_msg_type == 'admin') {
+			alerthtml = '';
+		}
 		if ( msg == '' ) {
 			alerthtml = '<p class="wps_order_msg_sent_notice">'+ wrael_common_param.message_empty +'</p><a href="" class="wps_remove_notice_msg">X</a>';
 		}
