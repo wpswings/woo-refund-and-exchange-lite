5436eca31c0ea4385610f8622d7aa3791d08d728
shivamddeveloper <shivamsrivastava@makewebbetter.com>
2022-11-28 17:58:14 +0530

    wpscan change

--- includes/woo-refund-and-exchange-lite-common-functions.php
+++ includes/woo-refund-and-exchange-lite-common-functions.php
@@ -145,7 +145,6 @@ if ( ! function_exists( 'wps_rma_lite_send_order_msg_callback' ) ) {
 						}
 						$sourcepath = sanitize_text_field( wp_unslash( $_FILES['wps_order_msg_attachment']['tmp_name'][ $i ] ) );
 						$f_name     = isset( $_FILES['wps_order_msg_attachment']['name'][ $i ] ) ? sanitize_text_field( wp_unslash( $_FILES['wps_order_msg_attachment']['name'][ $i ] ) ) : '';
-						$file_type  = isset( $_FILES['wps_order_msg_attachment']['type'][ $i ] ) ? sanitize_text_field( wp_unslash( $_FILES['wps_order_msg_attachment']['type'][ $i ] ) ) : '';
 						$targetpath = $directory . '/' . $order_id . '-' . $f_name;
 						$file_security = pathinfo( $f_name, PATHINFO_EXTENSION );
 						if ( 'png' === $file_security || 'jpeg' === $file_security || 'jpg' === $file_security ) {
