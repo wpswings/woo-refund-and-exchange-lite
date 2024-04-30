50732584d6826ea65722809d5b16454713f9b8ee
Mohd Danish <mohddanish@makewebetter.com>
2024-04-30 18:38:54 +0530

    lang-update

--- woocommerce-refund-and-exchange-lite.php
+++ woocommerce-refund-and-exchange-lite.php
@@ -24,7 +24,7 @@
  * Requires at least: 5.5.0
  * Tested up to: 6.5.2
  * WC requires at least: 6.5
- * WC tested up to: 8.8.2
+ * WC tested up to: 8.8.3
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
