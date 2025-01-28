0aaeb59d67017712f2301b930a1a5dcceb861a24
mohammaddanish-cedcoss <mohddanish@makewebbetter.com>
2025-01-28 14:13:09 +0530

    fixed-button-issue

--- public/class-woo-refund-and-exchange-lite-public.php
+++ public/class-woo-refund-and-exchange-lite-public.php
@@ -96,7 +96,7 @@ class Woo_Refund_And_Exchange_Lite_Public {
 	 * @param object $order is a current order.
 	 */
 	public function wps_rma_refund_button( $actions, $order ) {
-		if ( version_compare( WC()->version, '9.6.0', '>=' ) && is_account_page() && is_wc_endpoint_url('view-order') ) {
+		if ( ( version_compare( WC()->version, '9.6.0', '>=' ) && is_account_page() && is_wc_endpoint_url('view-order') ) || is_checkout() ) {
 			return $actions;
 		}
 		$show_refund_button = wps_rma_show_buttons( 'refund', $order );
