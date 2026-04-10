1071dc6fd1f1f559e32089300f155d12c9602570
ananya-cedcoss <ananyashukla@makewebbetter.com>
2026-04-10 15:07:54 +0530

    woocommerce version update

--- woocommerce-refund-and-exchange-lite.php
+++ woocommerce-refund-and-exchange-lite.php
@@ -23,9 +23,9 @@
  * Requires Plugins:  woocommerce
  *
  * Requires at least: 6.7.0
- * Tested up to: 6.9.1
+ * Tested up to: 6.9
  * WC requires at least: 6.5.0
- * WC tested up to: 10.5.1
+ * WC tested up to: 10.6.2
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
