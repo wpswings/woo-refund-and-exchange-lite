ce9d5594bbc6b917851c808549039a3f798dfb05
Shivamddeveloper <shivamsrivastava@makewebbetter.com>
2026-08-12 17:59:54 +0530

    loader issue solved

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '36d828d9d3a22caa611c');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '7478b4e8becfb660f516');
